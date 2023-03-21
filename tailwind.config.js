d07ccc94b4b8340bcef9b68dcf03d152be11710b
davidrondina <rondinadavid.r@gmail.com>
2023-03-21 16:09:31 +0800

    Route changes, Tailwind config file changes, image assets added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
     content: [
         "./resources/**/*.blade.php",
@@ -6,7 +9,20 @@ module.exports = {
         "./resources/**/*.vue",
       ],
   theme: {
-    extend: {},
+    extend: {
+        backgroundColor: ({ theme }) => ({
+            // 'sidebar': defaultTheme.backgroundColor.bg-green-800,
+            // 'btn': theme('theme.backgroundColor.bg-green-700'),
+            'sidebar': 'rgb(22 101 52)',
+            'btn': 'rgb(22 101 61)',
+        }),
+        fontFamily: {
+            'karla': ['karla', 'sans-serif']
+        },
+        colors: {
+            'regal-blue': '#243c5a',
+        },
+    },
   },
   plugins: [],
 }
