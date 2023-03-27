c806e05cda7113b9a9e4bc800ae8117df38dab93
davidrondina <rondinadavid.r@gmail.com>
2023-03-27 19:20:50 +0800

    Header changes

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
 <!-- Mobile Header -->
-<header x-data="{ isOpen: false }" class="z-10 w-full py-5 px-6  bg-theme-green text-white md:hidden shadow-lg fixed">
+<header x-data="{ isOpen: false }" class="z-10 w-full py-5 px-6  bg-theme-green text-white md:hidden shadow-lg sticky top-0">
     <div class="flex items-center justify-between">
         <a href="/" class="block font-semibold uppercase hover:text-gray-300">Manuela Homes Subdivision</a>
         <button @click="isOpen = !isOpen" class="text-white text-3xl focus:outline-none">
@@ -14,24 +14,28 @@
             Login
         </a>
         <a href="#" class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
-            News & Updates
+            First Link
         </a>
         <a href="#" class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
-            Officers
+            Second Link
         </a>
         <a href="#" class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
-            Contact
+            Third Link
         </a>
     </nav>
 </header>
 
 <!-- Desktop Header -->
-<header class="z-10 w-full py-2 px-6 sm:px-10 bg-theme-green text-white hidden md:flex md:justify-between shadow-lg fixed">
+<header class="z-10 w-full py-2 px-6 sm:px-10 bg-theme-green text-white hidden md:flex md:justify-between shadow-lg sticky top-0">
     <div class="w-auto flex items-center justify-between">
         <a href="/" class="block font-semibold uppercase hover:text-gray-300">Manuela Homes Subdivision</a>
     </div>
     <nav class="h-full">
         <ul class="h-full flex gap-8">
+            @auth
+                <li>Logged In!</li>
+            @endauth
+
             <li class="group w-[140px] p-3 rounded-lg text-center font-semibold nav-item"><a href="#" class="w-full opacity-75 group-hover:opacity-100">First Link</a></li>
             <li class="group w-[140px] p-3 rounded-lg text-center font-semibold nav-item"><a href="#" class="w-full opacity-75 group-hover:opacity-100">Second Link</a></li>
             <li class="group w-[140px] p-3 rounded-lg text-center font-semibold nav-item"><a href="#" class="w-full opacity-75 group-hover:opacity-100">Third Link</a></li>
